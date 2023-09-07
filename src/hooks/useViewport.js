f2fe3e598edef6fc59ba4894dcb54e90861061ac
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-09-07 16:10:56 -0700

    header start
    mui icon material dependency may not be needed for long. I will uninstall it if it is not needed later.
    🤤 I did not add the component to the app. Feel free to do so if you are ready.

---
+++ src/hooks/useViewport.js
@@ -0,0 +1,14 @@
+const { useState, useEffect } = require('react');
+
+const useViewPort = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return { width };
+};
+
+export default useViewPort;
