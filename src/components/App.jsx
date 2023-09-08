152741a7abce00bb13fa74a3c46631e3edcd767e
Jose Gonzales <jose.gonzales08@ymail.com>
2023-09-08 09:16:12 -0400

    [update to route login and register inside home]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-// import PrivateRoute from './PrivateRoute';
-// import PublicRoute from './PublicRoute';
 import Loader from './Loader/Loader';
 import Header from './Header';
 
 const Home = lazy(() => import('pages/Home/Home'));
-const Register = lazy(() => import('pages/Register/Register'));
-const Login = lazy(() => import('pages/Login/Login'));
 const Diary = lazy(() => import('pages/Diary/Diary'));
 const Calculator = lazy(() => import('pages/Calculator/Calculator'));
+const Register = lazy(() => import('pages/Register/Register'));
+const Login = lazy(() => import('pages/Login/Login'));
 
 export const App = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path='/' element={<Home />}>
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+          </Route>
           <Route path="/diary" element={<Diary />} />
           <Route path="/calculator" element={<Calculator />} />
+          <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </Suspense>
     </>
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,6 +4,7 @@ import { Dna } from 'react-loader-spinner';
 function Loader() {
   return (
     <Dna
+      sx={{margin: '0 auto',}}
       visible={true}
       height="200"
       width="200"
