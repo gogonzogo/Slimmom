bdf9ec604b9ff357a45594089911c2417215497d
jiml268 <jimlynch268@gmail.com>
2023-09-09 11:34:53 -0400

    remove logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Logo from '../Logo/Logo.jsx';
 import styles from './Header.module.css';
 // internal
 import useViewPort from 'hooks/useViewport';
@@ -22,12 +21,10 @@ const Header = () => {
         <Toolbar className={styles.headerToolbar}>
           {width > breakpoint ? (
             <div className={styles.logoTextContainer}>
-              <Logo className={styles.logo} />
               <Typography className={styles.fitMomText}>FitMom</Typography>
             </div>
           ) : (
             <IconButton>
-              <Logo className={styles.logo} />
             </IconButton>
           )}
 
