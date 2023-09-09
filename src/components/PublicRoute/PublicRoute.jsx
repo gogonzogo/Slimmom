32a36e3de0d384e7e22058bf32f08606cb253618
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-09-09 14:39:44 -0700

    public and private route components
    They are commented out until redux logic is implemented

---
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -0,0 +1,14 @@
+// import { useSelector } from "react-redux/es/hooks/useSelector";
+// import { Navigate } from 'react-router-dom';
+
+// import { isLoggedIn } from "redux/users/selectors";
+
+// const PrivateRoute = ({ children }) => {
+//     const logIn = useSelector(isLoggedIn);
+//     return (
+//         <div>
+//             {logIn ? children : <Navigate to="/" />}
+//         </div>)
+// };
+
+// export default PrivateRoute;
