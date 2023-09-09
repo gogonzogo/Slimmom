9fadaec58541acbfaa9e1a9cf379d1b19a0229f0
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-09-09 13:52:57 -0700

    update basename insert theme files
    delete backslash on basename
    add theme

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='Final-Team-Project/'> 
+    <BrowserRouter basename='Final-Team-Project'> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
