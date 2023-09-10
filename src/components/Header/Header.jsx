811a0a7cf8eccc9daad370a576c8e7cd3e35bb3a
Rachel Frederickson <vampirerachel@gmail.com>
2023-09-09 22:56:49 -0400

    merged with header to fix svg paths paths

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,13 @@
 
 // mui
 import { IconButton, useMediaQuery } from '@mui/material';
-import { IconButton, useMediaQuery } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import styles from './Header.module.css';
 import Logo from '../Logo/Logo'
-import styles from './Header.module.css';
+
 
 // internal
 import useViewPort from 'hooks/useViewport';
