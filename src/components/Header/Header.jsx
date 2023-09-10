df52d84f56b707277828cd163ba31b31f6e5b504
Rachel Frederickson <vampirerachel@gmail.com>
2023-09-09 22:39:21 -0400

    logo from figma

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 // external
 
 // mui
-import { IconButton } from '@mui/material';
+import { IconButton, useMediaQuery } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Logo from '../logo/Logo.jsx';
-import styles from './header.module.css';
+import styles from './Header.module.css';
+import Logo from '../Logo/Logo'
 // internal
 import useViewPort from 'hooks/useViewport';
 import Navigation from '../Navigation/Navigation.jsx';
@@ -15,19 +15,26 @@ import Navigation from '../Navigation/Navigation.jsx';
 const Header = () => {
   const { width } = useViewPort();
   const breakpoint = 320;
-
+  const isLargeScreen = useMediaQuery('(min-width: 769px)');
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar className={styles.headerToolbar}>
+    <Box sx={{ width: "100%" }}>
+      <AppBar position="static"
+        sx={{
+          boxShadow: 'none',
+          borderBottom: isLargeScreen ? 'none' : '2px solid #E0E0E0',
+
+        }}>
+        <Toolbar className={styles.headerToolbar}
+          sx={{justifyContent: isLargeScreen ? 'unset' : 'space-between'}}
+        >
           {width > breakpoint ? (
             <div className={styles.logoTextContainer}>
-              <Logo className={styles.logo} />
+              < Logo className={styles.logo}/>
               <Typography className={styles.fitMomText}>FitMom</Typography>
             </div>
           ) : (
-            <IconButton>
-              <Logo className={styles.logo} />
+              <IconButton>
+                <Logo className={styles.logo}/>
             </IconButton>
           )}
 
@@ -41,4 +48,3 @@ const Header = () => {
 };
 
 export default Header;
-
--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { ReactComponent as LogoSVG } from '../logo/logo.svg';
-import styles from './logo.module.css';
+import { ReactComponent as LogoSVG } from '../Logo/slimmom.svg';
+import { ReactComponent as SlimSVG } from "../Logo/slim.svg";
+import { ReactComponent as MomSVG } from "../Logo/mom.svg";
+import styles from './Logo.module.css';
 import { Link } from 'react-router-dom'
-
 const Logo = ({ className }) => { 
   return (
-    <Link to='../../pages/Diary' className={className || styles['logo-container']}>
-      <LogoSVG className={styles.logo} />
-    </Link>
+    <div className={className || styles['logo-container']}>
+      <div className={styles['vector-container']}>
+        <Link to='../../pages/Diary'>
+          <LogoSVG className={styles.logo} />
+        </Link>
+      </div>
+      <div className={styles['text-container']}>
+        <Link to='../../pages/Diary' className={styles['svg-link']}>
+          <SlimSVG className={styles.logo} />
+        </Link>
+        <Link to='../../pages/Diary' className={styles['svg-link']}>
+          <MomSVG className={styles.logo} />
+        </Link>
+      </div>
+    </div>
   );
 };
 
+
 export default Logo;
-
-
-
-
-
