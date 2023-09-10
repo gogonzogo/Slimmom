b9c5b87c7d85b02ac2639ecee45d4c08cf30e5c9
Jennifer Martinez <jmartinez414@gmail.com>
2023-09-10 03:10:23 -0500

    form styling updates

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,48 +1,70 @@
+import React from 'react';
+import { useState } from 'react';
+import style from '../LoginForm/login.module.css';
+
 import {
   Box,
   FormControl,
   TextField,
   Button,
-  Grid,
-  Container,
-  ButtonGroup,
+  Grid
 } from '@mui/material';
-import React from 'react';
 
-const buttons = [
-  <Button key={1} variant="contained" color="primary">
-    Log In
-  </Button>,
-  <Button key={2} variant="contained" color="primary">
-    Register
-  </Button>,
-];
+
 
 const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  // listens to input change
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: '100%' }} className={style.form_container}>
       <Grid align="center">
         {/* login form */}
-        <h2>LOG IN</h2>
+        <h2 style={{ color: '#FC842D', margin: '40px', fontFamily: 'Verdana', fontSize: '14px', fontWeight: '700'}}>LOG IN</h2>
         <FormControl variant="standard">
-          <TextField
-            variant="standard"
-            label="Email"
-            type="email"
-            fullWidth
-            required
-          />
-          <TextField
-            variant="standard"
-            label="Password"
-            type="password"
-            fullWidth
-            required
-          />
+          <form>
+            <TextField
+              variant="standard"
+              label="Email"
+              type="email"
+              name="email"
+              fullWidth
+              margin="normal"
+              required
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <TextField
+              variant="standard"
+              label="Password"
+              type="password"
+              name="password"
+              fullWidth
+              margin="normal"
+              required
+              value={formData.password}
+              onChange={handleChange}
+            />
+
+            <Box sx={{ marginTop: '60px', paddingBottom: '20px' }}>
+              <Button variant="contained" className={style.login_button}>
+                Log In
+              </Button>
+            </Box>
+            <Box>
+              <Button variant="contained" className={style.register_button} >
+                Register
+              </Button>
+            </Box>
+          </form>
         </FormControl>
-        <Grid>
-          <ButtonGroup orientation="vertical">{buttons}</ButtonGroup>
-        </Grid>
       </Grid>
     </Box>
   );
