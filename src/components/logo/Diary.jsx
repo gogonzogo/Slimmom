42af4101f85050ca6c3a1e15cfa4178cd3ed9a27
apav2086 <apav2086@gmail.com>
2023-09-10 03:37:16 -0400

    adding Calendar

--- src/components/logo/Diary.jsx
+++ src/components/logo/Diary.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import AllProductsList from 'components/productsList/allProductsList/AllProductsList';
 import DiaryAddProduct from 'components/DiaryAddProductForm/DiaryAddProductForm';
-import DiaryCalendar from 'components/DiaryCalendar/DiaryCalendar';
+
 function Diary() {
   return (
     <div>Diary
-      <DiaryCalendar />
       <DiaryAddProduct />
       <AllProductsList />
     </div>
---
+++ src/components/logo/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
+import { BrowserRouter } from "react-router-dom";
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename='Final-Team-Project/'> 
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
