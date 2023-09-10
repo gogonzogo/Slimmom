bbeb1a1ae8512805f0fee9a9caba20d6467d1982
itsjenm <jmartinez414@gmail.com>
2023-09-09 22:50:24 -0500

    login form updates

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,13 +1,51 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import {
+  Box,
+  FormControl,
+  TextField,
+  Button,
+  Grid,
+  Container,
+  ButtonGroup,
+} from '@mui/material';
+import React from 'react';
 
+const buttons = [
+  <Button key={1} variant="contained" color="primary">
+    Log In
+  </Button>,
+  <Button key={2} variant="contained" color="primary">
+    Register
+  </Button>,
+];
 
 const LoginForm = () => {
   return (
-    <Box>
-      <h1>Login Form</h1>
+    <Box sx={{ width: '100%' }}>
+      <Grid align="center">
+        {/* login form */}
+        <h2>LOG IN</h2>
+        <FormControl variant="standard">
+          <TextField
+            variant="standard"
+            label="Email"
+            type="email"
+            fullWidth
+            required
+          />
+          <TextField
+            variant="standard"
+            label="Password"
+            type="password"
+            fullWidth
+            required
+          />
+        </FormControl>
+        <Grid>
+          <ButtonGroup orientation="vertical">{buttons}</ButtonGroup>
+        </Grid>
+      </Grid>
     </Box>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
