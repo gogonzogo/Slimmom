73b7100bab915fdc8e7ae8526b9edaf03d930f41
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-09-11 14:57:27 -0700

    home header fix
    padding margin and gray line placement

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,8 +4,11 @@ import { ReactComponent as SlimSVG } from "../Logo/slim.svg";
 import { ReactComponent as MomSVG } from "../Logo/mom.svg";
 import styles from './Logo.module.css';
 import { NavLink } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
 
 const Logo = ({ className }) => { 
+  const isLargeScreen = useMediaQuery('(min-width: 769px)');
   return (
     <div className={className || styles['logo-container']}>
       <div className={styles['vector-container']}>
@@ -13,6 +16,12 @@ const Logo = ({ className }) => {
           <LogoSVG className={styles.logo} />
         </NavLink>
       </div>
+      <Box sx={{
+            paddingRight: isLargeScreen ? 3 : 0,
+            borderRight: isLargeScreen ? '2px solid #E0E0E0' : 'none',
+            marginRight: isLargeScreen ? 3 : 0,
+            marginTop: isLargeScreen ? 4 : 3,
+          }}>
       <div className={styles['text-container']}>
         <NavLink to='/Diary' className={styles['svg-link']}>
           <SlimSVG className={styles.logo} />
@@ -20,7 +29,8 @@ const Logo = ({ className }) => {
         <NavLink to='/Diary' className={styles['svg-link']}>
           <MomSVG className={styles.logo} />
         </NavLink>
-      </div>
+        </div>
+      </Box>
     </div>
   );
 };
