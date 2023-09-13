bcbe84acc90ca59a721b425a184b646a2b01e6c0
Rachel Frederickson <vampirerachel@gmail.com>
2023-09-12 22:34:58 -0400

    Redux Validation

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Box, FormControl, TextField, Button, Grid } from '@mui/material';
-import { toast } from 'react-toastify';
 import style from './RegistrationForm.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  updateFormData,
+  clearValidationReqs,
+} from '../../redux/validation/validationSlice'; // Import Redux actions
+
+import {
+  getValidationReqs,
+  getFormData,
+  getFormDataName,
+  getFormDataPassword,
+  getFormDataEmail,
+} from '../../redux/validation/validationSelectors'; // Import Redux selectors
+ // Import Redux actions and selectors
+import ValidationPopup from '../ValidationPopup/ValidationPopup';
 
 const RegistrationForm = () => {
+  const dispatch = useDispatch();
+
+  const validationReqs = useSelector(getValidationReqs);
+
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,10 +35,10 @@ const RegistrationForm = () => {
     password: '',
   });
 
- // listens to input change
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    dispatch(updateFormData({ [name]: value })); // Dispatch the action to update Redux store
   };
 
   const handleSubmit = (e) => {
@@ -29,37 +47,24 @@ const RegistrationForm = () => {
     let hasErrors = false;
     const newErrors = { ...errors };
 
-    if (!formData.name) {
-      newErrors.name = 'Must be at least 3 characters long';
-      toast.error('Name is required');
-      hasErrors = true;
-    } else if (formData.name.length < 3) {
-      newErrors.name = 'Name must be at least 3 characters long';
-      toast.error('Name must be at least 3 characters long');
+    // Validate form fields based on Redux validation requirements
+    // Example validation based on validationReqs - modify this based on your actual validation requirements
+    if (validationReqs.name.some((req) => !req.met)) {
+      newErrors.name = 'Name validation failed';
       hasErrors = true;
     } else {
       newErrors.name = '';
     }
 
-    if (!formData.email) {
-      newErrors.email = 'Please enter a valid email address';
-      toast.error('Email is required');
-      hasErrors = true;
-    } else if (!isValidEmail(formData.email)) {
-      newErrors.email = 'Please enter a valid email address: example@mail.com';
-      toast.error('Please enter a valid email address');
+    if (validationReqs.email.some((req) => !req.met)) {
+      newErrors.email = 'Email validation failed';
       hasErrors = true;
     } else {
       newErrors.email = '';
     }
 
-    if (!formData.password) {
-      newErrors.password = 'Must be at least 8 characters long';
-      toast.error('Password is required');
-      hasErrors = true;
-    } else if (formData.password.length < 8) {
-      newErrors.password = 'Password must be at least 8 characters long';
-      toast.error('Password must be at least 8 characters long');
+    if (validationReqs.password.some((req) => !req.met)) {
+      newErrors.password = 'Password validation failed';
       hasErrors = true;
     } else {
       newErrors.password = '';
@@ -74,72 +79,97 @@ const RegistrationForm = () => {
       setFormData({ name: '', email: '', password: '' });
 
       // Show a success message
-      toast.success('Registration successful!');
+      // toast.success('Registration successful!');
     }
   };
 
-  const isValidEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  const renderValidationPopup = () => {
+    return <ValidationPopup validationData={validationReqs.name} />;
   };
-
+  const areAllValidationFieldsValid = () => {
+    return (
+      validationReqs.name.every((req) => req.meet) &&
+      validationReqs.email.every((req) => req.meet) &&
+      validationReqs.password.every((req) => req.meet)
+    )
+  };
   return (
     <Box sx={{ width: '100%' }} className={style.form_container}>
       <Grid align="center">
         {/* registration form */}
-        <h2 style={{ color: '#FC842D', margin: '40px', fontFamily: 'Verdana', fontSize: '14px', fontWeight: '700'}}>REGISTER</h2>
+        <h2
+          style={{
+            color: '#FC842D',
+            margin: '40px',
+            fontFamily: 'Verdana',
+            fontSize: '14px',
+            fontWeight: '700',
+          }}
+        >
+          REGISTER
+        </h2>
         <FormControl variant="standard">
           <form onSubmit={handleSubmit} noValidate>
-<TextField
-  variant="standard"
-  label={"Name *"}
-  type="text"
-  name="name"
-  fullWidth
-  margin="normal"
-  value={formData.name}
-  onChange={handleChange}
-  error={!!errors.name} // Set error prop to true when there is an error
-  helperText={errors.name ? errors.name : ''} // Display error message as helper text
-/>
+            <TextField
+              variant="standard"
+              label={'Name *'}
+              type="text"
+              name="name"
+              fullWidth
+              margin="normal"
+              value={formData.name}
+              onChange={handleChange}
+              error={!!errors.name}
+              helperText={errors.name ? errors.name : ''}
+            />
 
-<TextField
-  variant="standard"
-  label={"Email *"}
-  type="email"
-  name="email"
-  fullWidth
-  margin="normal"
-  value={formData.email}
-  onChange={handleChange}
-  error={!!errors.email} // Set error prop to true when there is an error
-  helperText={errors.email ? errors.email : ''} // Display error message as helper text
-/>
+            <TextField
+              variant="standard"
+              label={'Email *'}
+              type="email"
+              name="email"
+              fullWidth
+              margin="normal"
+              value={formData.email}
+              onChange={handleChange}
+              error={!!errors.email}
+              helperText={errors.email ? errors.email : ''}
+            />
 
-<TextField
-  variant="standard"
-  label={"Password *"}
-  type="password"
-  name="password"
-  fullWidth
-  margin="normal"
-  value={formData.password}
-  onChange={handleChange}
-  error={!!errors.password} // Set error prop to true when there is an error
-  helperText={errors.password ? errors.password : ''} // Display error message as helper text
-/>
-
+            <TextField
+              variant="standard"
+              label={'Password *'}
+              type="password"
+              name="password"
+              fullWidth
+              margin="normal"
+              value={formData.password}
+              onChange={handleChange}
+              error={!!errors.password}
+              helperText={errors.password ? errors.password : ''}
+            />
 
             <Box sx={{ marginTop: '20px', paddingBottom: '20px' }}>
-              <Button variant="contained" type="submit" className={style.register_button}>
+              <Button
+                variant="contained"
+                type="submit"
+                className={style.register_button}
+                disabled={!areAllValidationFieldsValid()}
+              >
                 Register
               </Button>
             </Box>
           </form>
         </FormControl>
+
+        {/* Render ValidationPopup component */}
+        {renderValidationPopup()}
+
         <Box sx={{ marginTop: '20px', paddingBottom: '20px' }}>
           <Link to="/Login">
-            <Button variant="contained" className={style.login_button}>Log In</Button>
+            <Button variant="contained" className={style.login_button}>
+              Log In
+            </Button>
           </Link>
         </Box>
       </Grid>
