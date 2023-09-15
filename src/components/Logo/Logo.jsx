e32413c2fab030c1627f4069ee67783529d5b332
gogonzogo <114868158+gogonzogo@users.noreply.github.com>
2023-09-14 20:37:03 -0400

    Revert "Revert "work on userinfo/ nav/ header/logo components""

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -35,17 +35,4 @@ const Logo = ({ className }) => {
   );
 };
 
-const TextLogo = () => {
-  return (
-    <div className={styles['text-logo-container']}>
-        <NavLink to='/Diary' className={styles['svg-link']}>
-          <SlimSVG />
-        </NavLink>
-        <NavLink to='/Diary' className={styles['svg-link']}>
-          <MomSVG />
-        </NavLink>
-        </div>
-  );
-};
-
-export {Logo, TextLogo};
+export default Logo;
