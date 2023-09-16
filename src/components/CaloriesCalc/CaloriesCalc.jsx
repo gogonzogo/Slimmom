a639b938c13ffa4bf8b3c9ca70b0fa517f8a296f
jiml268 <jimlynch268@gmail.com>
2023-09-15 21:11:08 -0400

    calculator almost done

--- src/components/CaloriesCalc/CaloriesCalc.jsx
+++ src/components/CaloriesCalc/CaloriesCalc.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import css from './CaloriesCalc.module.css';
 import { useState } from 'react';
+import {useSelector, useDispatch } from 'react-redux';
 
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
@@ -10,8 +11,40 @@ import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Modal from 'components/Modal/Modal';
+import ValidationPopup from '../ValidationPopup/ValidationPopup';
+import { validateHeight, validateAge, validateCurrent, validateDesired, validateBlood }
+from '../../redux/validation/calculateCalsSlice'
 
 const CaloriesCalc = () => {
+  const dispatch = useDispatch();
+  const validHeight = useSelector((state) => state.calculate.isHeightValid)
+  const validAge = useSelector(state => state.calculate.isAgeValid)
+  const validcurrent = useSelector(state => state.calculate.isCurrentValid)
+  const validDesired = useSelector(state => state.calculate.isDesiredValid)
+  const validBlood = useSelector(state => state.calculate.isBloodValid)
+
+
+  const isFormValid = validHeight && validAge && validcurrent && validDesired && validBlood ? true : false 
+
+
+  
+  const [validationPopups, setValidationPopups] = useState({
+    weight: false,
+    age: false,
+    currentWeight: false,
+    desiredWeight: false,
+    bloodType: false
+  });
+  const [focusedField, setFocusedField] = useState(null);
+
+  const toggleValidationPopup = (fieldName, visible) => {
+    setValidationPopups({ ...validationPopups, [fieldName]: visible });
+    console.log('isFormValid', isFormValid)
+  };
+
+    const validationReqs = useSelector((state) => state.calculate.validationReqs);
+
+
   const [modalState, setModalState] = useState({
     open: false,
     totalCalories: null,
@@ -38,33 +71,69 @@ const CaloriesCalc = () => {
     }, 250);
   };
 
-  const [stats, setStats] = useState({
+  const [formData, setFormData] = useState({
     height: '',
     age: '',
     currentWeight: '',
     desiredWeight: '',
     bloodType: '',
   });
-  const { height, age, currentWeight, desiredWeight, bloodType } = stats;
 
   const changeHandler = e => {
-    setStats({ ...stats, [e.target.name]: [e.target.value] });
+const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  switch (name) {
+    case 'height':
+      dispatch(validateHeight({ fieldValue: value }));
+        break;
+    case 'age':
+      dispatch(validateAge({ fieldValue: value }));
+        break;
+    case 'currentWeight':
+      dispatch(validateCurrent({ fieldValue: value }));
+        break;
+    case 'desiredWeight':
+      dispatch(validateDesired({ fieldValue: value }));
+      break;
+    case 'bloodType':
+      dispatch(validateBlood({ fieldValue: value }));
+      // console.log('bloodType' )
+      break;
+    default:
+        break;
+    }
+    setFocusedField(name);
+    toggleValidationPopup(name, true);
+  };
+
+  const renderValidationPopup = () => {
+    console.log('focusedField', focusedField)
+    return (
+      <ValidationPopup
+        validationData={validationReqs[focusedField]}
+        visible={!!focusedField}
+      />
+    );
   };
 
   const submitHandler = e => {
     e.preventDefault();
+    const {height, age, currentWeight, desiredWeight, } = formData
     const totalCalories =
       10 * currentWeight +
       6.25 * height -
       5 * age -
       161 -
-      10 * (currentWeight - currentWeight);
-    console.log(height);
-    console.log(age);
-    console.log(currentWeight);
-    console.log(desiredWeight);
-    console.log(bloodType);
-    console.log(totalCalories);
+      10 * (currentWeight - desiredWeight);
+    // console.log(height);
+    // console.log(age);
+    // console.log(currentWeight);
+    // console.log(desiredWeight);
+    // console.log(bloodType);
+    // console.log(totalCalories);
 
     handleOpen(totalCalories); // give me food
   };
@@ -90,20 +159,17 @@ const CaloriesCalc = () => {
                     width: '240px',
                     paddingRight: '32px',
                   }}
+                                InputLabelProps={{ style: { color: "#9B9FAA" } }}
+
                   type="tel"
                   inputprops={{ inputprops: { min: 122, max: 214 } }}
-                  label="Height"
+                  label="Height *"
                   variant="standard"
                   onChange={changeHandler}
-                  value={height}
+                  value={formData.height}
                   name="height"
-                  required
-                  error={height !== '' && (height < 122 || height > 214)}
-                  helperText={
-                    height !== '' && (height < 122 || height > 214)
-                      ? 'Height must be between 122cm and 214cm.'
-                      : ' '
-                  }
+              onFocus={() => setFocusedField('height')}
+              onBlur={() => setFocusedField(null)}
                 />
                 <TextField
                   sx={{
@@ -116,20 +182,16 @@ const CaloriesCalc = () => {
                     width: '240px',
                     paddingRight: '32px',
                   }}
+                  InputLabelProps={{ style: { color: "#9B9FAA" } }}
                   type="tel"
                   inputprops={{ inputprops: { min: 18, max: 80 } }}
                   label="Age"
                   variant="standard"
                   onChange={changeHandler}
-                  value={age}
+                  value={formData.age}
                   name="age"
-                  required
-                  error={age !== '' && (age < 18 || age > 80)}
-                  helperText={
-                    age !== '' && (age < 18 || age > 80)
-                      ? 'Age must be between 18 and 80.'
-                      : ' '
-                  }
+                  onFocus={() => setFocusedField('age')}
+              onBlur={() => setFocusedField(null)}
                 />
                 <TextField
                   sx={{
@@ -142,24 +204,17 @@ const CaloriesCalc = () => {
                     width: '240px',
                     paddingRight: '32px',
                   }}
+                                InputLabelProps={{ style: { color: "#9B9FAA" } }}
+
                   type="tel"
                   inputprops={{ inputprops: { min: 34, max: 181 } }}
                   label="Current Weight"
                   variant="standard"
                   onChange={changeHandler}
-                  value={currentWeight}
+                  value={formData.currentWeight}
                   name="currentWeight"
-                  error={
-                    currentWeight !== '' &&
-                    (currentWeight < 34 || currentWeight > 181)
-                  }
-                  helperText={
-                    currentWeight !== '' &&
-                    (currentWeight < 34 || currentWeight > 181)
-                      ? 'Current Weight must be between 34kg and 181kg.'
-                      : ' '
-                  }
-                  required
+                  onFocus={() => setFocusedField('currentWeight')}
+              onBlur={() => setFocusedField(null)}
                 />
               </div>
               <div className={css.formdiv}>
@@ -174,28 +229,17 @@ const CaloriesCalc = () => {
                     width: '240px',
                     paddingRight: '32px',
                   }}
+                                InputLabelProps={{ style: { color: "#9B9FAA" } }}
+
                   type="tel"
                   inputprops={{ inputprops: { min: 34, max: 181 } }}
                   label="Desired Weight"
                   variant="standard"
                   onChange={changeHandler}
-                  value={desiredWeight}
+                  value={formData.desiredWeight}
                   name="desiredWeight"
-                  required
-                  error={
-                    desiredWeight !== '' &&
-                    (desiredWeight < 34 ||
-                      desiredWeight > 181 ||
-                      desiredWeight > currentWeight)
-                  }
-                  helperText={
-                    desiredWeight !== '' &&
-                    (desiredWeight < 34 ||
-                      desiredWeight > 181 ||
-                      desiredWeight > currentWeight)
-                      ? 'Desired Weight must be between 34kg and 181kg. & less then current weight'
-                      : ' '
-                  }
+                  onFocus={() => setFocusedField('desiredWeight')}
+              onBlur={() => setFocusedField(null)}
                 />
                 <FormLabel id="demo-radio-buttons-group-label">
                   Blood Type
@@ -203,7 +247,7 @@ const CaloriesCalc = () => {
 
                 <RadioGroup
                   aria-labelledby="demo-controlled-radio-buttons-group"
-                  value={bloodType}
+                  value={formData.bloodType}
                   name="bloodType"
                   sx={{
                     '&, &.Mui-checked': {
@@ -212,7 +256,8 @@ const CaloriesCalc = () => {
                     flexDirection: 'row',
                   }}
                   onChange={changeHandler}
-                  required
+              onFocus={() => setFocusedField('bloodType')}
+              onBlur={() => setFocusedField(null)}
                 >
                   <FormControlLabel
                     value="A"
@@ -261,6 +306,8 @@ const CaloriesCalc = () => {
                 </RadioGroup>
               </div>
 
+              {renderValidationPopup()}
+
               <Button
                 sx={{
                   margin: '40px 0',
@@ -279,6 +326,7 @@ const CaloriesCalc = () => {
                 }}
                 type="submit"
                 variant="contained"
+                disabled={!isFormValid}
               >
                 Start losing weight
               </Button>
---
+++ src/redux/validation/calculateCalsSelectors.js
@@ -0,0 +1,62 @@
+import { createSelector } from 'reselect';
+
+const selectCalculation = (state) => state.calculate;
+
+export const selectAgeValidationReqs = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.validationReqs.age
+);
+
+export const selectCurrentValidationReqs = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.validationReqs.currentWeight
+);
+
+export const selectDesiredValidationReqs = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.validationReqs.desiredWeight
+);
+
+export const selectHeightValidationReqs = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.validationReqs.height
+);
+
+export const selectHBloodReqs = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.validationReqs.blood
+);
+
+export const selectAgeValid = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.isAgeValid
+);
+
+
+export const selectIsHeightValid = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.IsHeightValid
+);
+
+
+
+export const selectCurrentValid = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.isCurrentValid
+);
+export const selectDesiredValid = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.isDesiredValid
+);
+
+export const selectBloodValid = createSelector(
+  [selectCalculation],
+  (calculate) => calculate.isBloodValid
+);
+
+export const selectFormIsValid = createSelector(
+    [selectIsHeightValid, selectAgeValid, selectCurrentValid, selectDesiredValid, selectBloodValid],
+  ( isHeightValid,isAgeValid, isCurrentValid, isDesiredValid, isBloodValid ) =>
+     isHeightValid && isAgeValid && isCurrentValid && isDesiredValid && isBloodValid
+);
+
