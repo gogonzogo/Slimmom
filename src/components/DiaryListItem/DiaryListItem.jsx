8e376c04346d48ad2c397829d40542037f92c5e8
Jose Gonzales <jose.gonzales08@ymail.com>
2023-09-16 08:20:39 -0400

    [calories bug fix]

--- src/components/DiaryListItem/DiaryListItem.jsx
+++ src/components/DiaryListItem/DiaryListItem.jsx
@@ -5,45 +5,34 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Divider from '@mui/material/Divider';
 
-
-
-
-
-function ProductListItem({ product , handleDelete }) {
+function ProductListItem({ product, handleDelete }) {
   return (
     <ListItem
-                  secondaryAction={
-                    <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(product.productId)}>
-                      <DeleteIcon  />
-                    </IconButton>
-                  }
+      secondaryAction={
+        <IconButton
+          edge="end"
+          aria-label="delete"
+          onClick={() => handleDelete(product.productId)}
+        >
+          <DeleteIcon />
+        </IconButton>
+      }
     >
-
-      <ListItemText sx={{mr:6, px:3}} >
-      <ListItemText
-                     primary={product.title} 
-      />
+      <ListItemText sx={{ mr: 6, px: 3 }}>
+        <ListItemText primary={product.title} />
       </ListItemText>
-      <Divider variant="insert"  />
+      <Divider variant="insert" />
 
-
-      
       <ListItemText sx={{ mr: 4, px: 2 }}>
-      <ListItemText
-                     primary={product.weight} 
-      />
+        <ListItemText primary={product.weight} />
       </ListItemText>
-      <Divider variant="insert"  />
+      <Divider variant="insert" />
 
-      
-                  <ListItemText sx={{mr:4, px:2}} >
-      <ListItemText
-                     primary={product.calories} 
-      />
+      <ListItemText sx={{ mr: 4, px: 2 }}>
+        <ListItemText primary={product.calories} />
       </ListItemText>
-      <Divider variant="insert"  />
-        
-                </ListItem>
+      <Divider variant="insert" />
+    </ListItem>
     // <li  className={style.productListItem}>
     //   <span>
     //    ({product.title}) - ({product.weight}) - ({product.calories})
