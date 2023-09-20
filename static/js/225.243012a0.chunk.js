0e1279580d575396795ea6b75bcffa2aa4f0b82a
gogonzogo <114868158+gogonzogo@users.noreply.github.com>
2023-09-20 14:51:11 +0000

    Deploying to gh-pages from @ gogonzogo/Final-Team-Project@881c0323d6842eb6c29f4911502e1e8cc31bea7b 🚀

---
+++ static/js/225.243012a0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinal_team_project=self.webpackChunkfinal_team_project||[]).push([[225],{9544:function(a,e,n){n.r(e);var c=n(9357),s=(n(2791),n(7689)),r=n(184);e.default=function(){return(0,r.jsxs)("div",{className:" background mainBackground",children:[(0,r.jsx)(c.Z,{}),(0,r.jsx)(s.j3,{})]})}}}]);
+//# sourceMappingURL=225.243012a0.chunk.js.map
