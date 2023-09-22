d68411e02993bf31413d390601cfd31281883110
Rachel Frederickson <vampirerachel@gmail.com>
2023-09-22 00:04:12 -0400

    Updated Logo Styles

--- src/components/Logo/ImageLogo.jsx
+++ src/components/Logo/ImageLogo.jsx
@@ -6,9 +6,9 @@ import { NavLink } from 'react-router-dom';
 const ImageLogo = ({ className }) => {
   return (
     <div className={className || styles['logo-container']}>
-      <div className={styles['image-container']}>
+      <div className={styles['vector-container']}>
         <NavLink to='/Diary'>
-          <LogoSVG className={styles.logo} width="71" height="71" alt="A waistline with a green measuring tape"/>
+          <LogoSVG className={styles.logo} width="100" height="100" alt="A waistline with a green measuring tape"/>
         </NavLink>
       </div>
     </div>
