42ff9dd83c8d97f2ad676a6ce53cdcb383e4a89b
Amira <105366233+amira3a@users.noreply.github.com>
2023-09-26 11:09:56 +0300

    Update-diary-operation-redux

--- src/redux/diary/diaryOperations.js
+++ src/redux/diary/diaryOperations.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+axios.defaults.baseURL = "http://localhost:3030/api";
+
 export const fetchDiary = createAsyncThunk(
   'diary/fetchDiary',
-  async (date, rejectWithValue) => {
+  async (date,  rejectWithValue) => {
     try {
-      const response = await axios.post('day/info', {date: date,});
+      const response = await axios.post('/day/info', {date: date});
+      console.log(response.data.data)
       return response.data.data;
     } catch (error) {
       return rejectWithValue(error.message);
@@ -16,15 +19,18 @@ export const fetchDiary = createAsyncThunk(
 export const addDiaryEntry = createAsyncThunk(
   'diary/addDiary',
   async (data, rejectWithValue) => {
+    console.log('addDiaryData', data)
     const newDiaryEntry = {
       date: data.calDate,
       title: data.productName,
       weight: data.grams,
       calories: data.calories,
     };
+    console.log('addDiaryNewEntry', newDiaryEntry);
     try {
-      const response = await axios.post('users/addFood', newDiaryEntry);
-      return response.data.data;
+      const response = await axios.post('/users/addFood', `${JSON.parse(newDiaryEntry)}`);
+      console.log(response.data);
+      return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
     }
@@ -35,8 +41,9 @@ export const deleteDiaryEntry = createAsyncThunk(
   'diary/deleteDiary',
   async (data, rejectWithValue) => {
     try {
+      console.log(data);
       const response = await axios.delete(
-        `users/deleteFood/${JSON.stringify(data)}`
+        `/users/deleteFood/${JSON.stringify(data)}`
       );
       return response.data;
     } catch (error) {
@@ -50,7 +57,7 @@ export const searchFoods = createAsyncThunk(
   async (data, rejectWithValue) => {
     try {
       const response = await axios.get(
-        `food/query/${data}`
+        `/food/query/${data}`, `${JSON.stringify(data)}`
       );
       return response.data.data;
     } catch (error) {
