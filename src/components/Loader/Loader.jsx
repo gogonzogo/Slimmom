fccd250ab54f6c29c6f7dc290a7a6737e0d3544f
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-09-25 23:03:19 -0700

    replace dna with hearts ?

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Dna } from 'react-loader-spinner';
+import { Hearts } from 'react-loader-spinner';
 
 function Loader() {
   return (
-    <Dna
-      sx={{margin: '0 auto',}}
-      visible={true}
+    // <Dna
+    //   sx={{margin: '0 auto',}}
+    //   visible={true}
+    //   height="200"
+    //   width="200"
+    //   ariaLabel="dna-loading"
+    //   wrapperStyle={{}}
+    //   wrapperClass="dna-wrapper"
+    // />
+
+    // If the color is better orange sub this for the green:  "var(--accent-bright-color)"
+    <Hearts 
       height="200"
       width="200"
-      ariaLabel="dna-loading"
+      color="#4fa94d"
+      ariaLabel="hearts-loading"
       wrapperStyle={{}}
-      wrapperClass="dna-wrapper"
-    />
+      wrapperClass=""
+      visible={true}
+/>
   );
 }
 
