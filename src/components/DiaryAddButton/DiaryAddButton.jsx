6b2c4fcd0c8be30a694f0126511694bb3213bca8
Amira <105366233+amira3a@users.noreply.github.com>
2023-09-28 10:04:19 +0300

    Button add products-disabled

--- src/components/DiaryAddButton/DiaryAddButton.jsx
+++ src/components/DiaryAddButton/DiaryAddButton.jsx
@@ -2,13 +2,14 @@ import css from './DiaryAddButton.module.css';
 import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
 
-export default function DiaryAddButton({ onClick }) {
+export default function DiaryAddButton({ onClick , disabled }) {
   return (
     <IconButton
       variant="outlined"
       className={css.CirclePlus}
       type="button"
       onClick={onClick}
+      disabled= {disabled}
     >
       <AddIcon />
     </IconButton>
