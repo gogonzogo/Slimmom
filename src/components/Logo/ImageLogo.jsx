d244f5bcc75452e1c99be27a3ec8b8a921dbf491
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-10-13 12:35:46 -0700

    conditionally render dark theme icon
    create new icon component
    create sprite

--- src/components/Logo/ImageLogo.jsx
+++ src/components/Logo/ImageLogo.jsx
@@ -3,21 +3,19 @@ import { useSelector } from 'react-redux';
 import { selectThemeMode } from 'redux/theme/themeSelectors';
 import { ReactComponent as LogoSVG } from './slimmom.svg';
 import styles from './ImageLogo.module.css';
-import sprite from '../Logo/sprite.svg';
+import CustomIcon from 'components/CustomIcon/CustomIcon';
 
 const ImageLogo = () => {
   const themeMode = useSelector(selectThemeMode);
-  console.log(themeMode);
   return (
     <div className={styles['imagelogo-container']}>
       {themeMode === 'dark'
-        ? (<svg
-              fill="currentColor"
-              className={styles.imageLogo}
+        ? (<CustomIcon
+              iconName ="slimMomDark"
+              className={`${styles.imageLogo}`}
+              // width="24" height="24"
               alt="A waistline with a green measuring tape"
-            >
-              <use xlinkHref={`${sprite}.svg#slimMomDark`}/>
-            </svg>
+            />
         ) : (
             <LogoSVG
               className={styles.imageLogo}
